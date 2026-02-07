3f4a1290f44d38bc4565861fd2bdbee79d699f07
Ediki Shengelia <shengelia301@gmail.com>
2026-02-07 16:26:56 +0400

    factory and seeder is done

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,7 @@ class Review extends Model
 {
     /** @use HasFactory<\Database\Factories\ReviewFactory> */
     use HasFactory;
+    protected $fillable = ['review', 'rating'];
     public function book()
     {
         return $this->belongsTo(Book::class);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,30 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        \App\Models\Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            \App\Models\Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+        \App\Models\Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            \App\Models\Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+        \App\Models\Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            \App\Models\Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }
